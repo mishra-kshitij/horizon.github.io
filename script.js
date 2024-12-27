adc870bcc700198d8e30bb2ff1c3d916bb3ea502
mishra-kshitij <148680460+mishra-kshitij@users.noreply.github.com>
2024-12-27 16:28:03 +0530

    Consolidated File System Split
    Managed and broke down code as per the relevant file extension for cleaner organization.

---
+++ script.js
@@ -0,0 +1,11 @@
+function sendEmail() {
+    window.location.href = "mailto:kshitij.mishra@hotmail.com";
+}
+
+function openMorsePage() {
+    window.open('morse-translator.html', '_blank');
+}
+
+function openZedaIn() {
+    window.open('https://zeda.in', '_blank');
+}
